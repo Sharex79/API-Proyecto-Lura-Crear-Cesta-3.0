5e07a601d912e5a80ab6944fc541897530b7ef06
Sharex79 <sharex79@gmail.com>
2025-09-29 19:29:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,13 +34,12 @@ app.post("/api/crear_cesta", async (req, res) => {
   }
 });
 
-// Endpoint para obtener todos los productos en la cesta con su título y foto
 app.get("/api/cestas", async (req, res) => {
   try {
     const result = await pool.query(`
       SELECT cp.cantidad_producto, p.id AS id_producto, p.titulo, p.imagen1
       FROM cestas_productos cp
-      JOIN productos p ON cp.id_producto = p.id
+      JOIN public.productos p ON cp.id_producto = p.id
     `);
 
     res.json(result.rows);
@@ -50,6 +49,8 @@ app.get("/api/cestas", async (req, res) => {
 });
 
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API escuchando en puerto ${PORT}`));
+
 
